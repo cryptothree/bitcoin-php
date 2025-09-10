7c4cc4a3d2dee5bebf5a55679cba28d9dbf4f03e
CryptoThree <408445497@qq.com>
2025-09-10 11:42:43 +0800

    fix: implement getSignatureType method for paragonie/ecc 2.5 compatibility

--- src/Crypto/EcAdapter/Impl/PhpEcc/Signature/Signature.php
+++ src/Crypto/EcAdapter/Impl/PhpEcc/Signature/Signature.php
@@ -85,4 +85,14 @@ class Signature extends Serializable implements SignatureInterface, \Mdanter\Ecc
     {
         return (new DerSignatureSerializer($this->ecAdapter))->serialize($this);
     }
+
+    /**
+     * Returns "ecdsa" or "schnorr" depending on the signature type.
+     *
+     * @return string
+     */
+    public function getSignatureType(): string
+    {
+        return "ecdsa";
+    }
 }
